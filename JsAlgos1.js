ff4b54e778d48642a2fd4936ac9891f594f663bc
michelle <njerimwanjiku@gmail.com>
2021-01-25 19:25:43 +0300

    algos

--- JsAlgos1.js
+++ JsAlgos1.js
@@ -19,7 +19,6 @@ function sumUp2(n){
 function anArray( array){
     let arr =[];
     let sum= 0;
-
 }
 anArray([2,5,7])
 
@@ -44,14 +43,12 @@ for (let i = 0; i<arr.length; i++){
 }
   return result;
 }
-
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 //check if a   ends with a specified word. negative length menas its going to start fromend and not start like it would normally do.
 function confirmEnding(str, target) {
   return str.slice(-target.length)=== target;
 }
-
 confirmEnding("Bastian", "n");
 
 //repeating a string 
@@ -64,8 +61,8 @@ function repeatStringNumTimes(str, num) {
 
   return rstr;
 }
-
 repeatStringNumTimes("abc", 3);
+
 //truncating a string 
 function truncateString(str, num) {
   if (str.length > num){
@@ -75,6 +72,30 @@ function truncateString(str, num) {
     return str;
   }
   }
-  
-  truncateString("A-tisket a-tasket A green and yellow basket", 8);
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+//find if elements in an array pass tru to function rules.
+function findElement(arr, func) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++){
+   num = arr[i];
+   if (func(num)){
+     return num
+   }
+  }
+   return undefined
+  }
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+//checcking if value of bool is boolean or not 
+function booWho(bool) {
+  if (typeof bool === "boolean"){
+    return true
+  }else{
+  return false;
+}
+}
+booWho(null);
+
+
 
